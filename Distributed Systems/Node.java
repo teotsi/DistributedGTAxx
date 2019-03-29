3f925fbecd52203832dae0230e1a7acbe62063d4
teotsi <teotsi@gmail.com>
2019-03-29 12:29:34 +0200

    test

--- Distributed Systems/Node.java
+++ Distributed Systems/Node.java
@@ -1,19 +1,42 @@
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.List;
 
 public class Node {
 
     List<Broker> brokers;
+    ServerSocket server;
+    Socket connectionSocket;
 
-    public Node(List<Broker> list){
+    public Node(List<Broker> list) {
         this.brokers = list;
     }
 
 
-    public void init(int value){
-        Socket socket = new Socket(value);
+    public void init(int port) {
+        try {
+            server = new ServerSocket(port); //initialising server
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
-    public void connect(){}
-    public void disconnect(){}
-    public void updateNodes(){}
+
+    public void connect() {
+        while(true){
+            try {
+                connectionSocket = server.accept(); //accepting connection
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void disconnect() {
+    }
+
+    public void updateNodes() {
+    }
 }
