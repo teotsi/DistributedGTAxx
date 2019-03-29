94aa10f4abfa2fbd51a5487710047e1a551aa9b2
Family <teotsi@gmail.com>
2019-03-29 23:43:12 +0200

    kind of works with 1 pub/sub

--- Distributed Systems/Publisher.java
+++ Distributed Systems/Publisher.java
@@ -1,4 +1,6 @@
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.security.NoSuchAlgorithmException;
@@ -9,6 +11,9 @@ import java.security.MessageDigest;
 public class Publisher extends Node {
     ServerSocket server;
     Socket connectionSocket;
+    ObjectOutputStream out;
+    ObjectInputStream in;
+    int[] array = {81, 52, 34, 42, 54, 67, 57, 18, 29};
     public Publisher(List<Broker> brokers) {
         super(brokers);
 
@@ -17,14 +22,34 @@ public class Publisher extends Node {
     @Override
     public void init(int port){
         try {
-            server = new ServerSocket(port); //initialising server
-            while(true){
-                connectionSocket = server.accept();
-            }
+            server = new ServerSocket(4321); //initialising server
         } catch (IOException e) {
             e.printStackTrace();
         }
+        connect();
     }
+
+    @Override
+    public void connect(){
+        while(true){
+            try {
+                connectionSocket = server.accept();
+
+            out = new ObjectOutputStream(connectionSocket.getOutputStream());
+            in = new ObjectInputStream(connectionSocket.getInputStream());
+            int request = Integer.parseInt( (String) in.readObject());
+            int response = array[request];
+            push(new Topic((String.valueOf(response))),null);
+            connectionSocket.close();
+            server.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (ClassNotFoundException e){
+                e.printStackTrace();
+            }
+        }
+    }
+
     public void getBrokerList() {
     }
 
@@ -39,7 +64,9 @@ public class Publisher extends Node {
         return new Broker(new ArrayList<Subscriber>(), new ArrayList<Publisher>(), null);
     }
 
-    public void push(Topic t, Value v) {
+    public void push(Topic t, Value v) throws IOException {
+        out.writeObject(t.getBusLine());
+        out.flush();
     }
 
     public void notifyFailure(Broker b) {
--- Distributed Systems/Subscriber.java
+++ Distributed Systems/Subscriber.java
@@ -1,12 +1,33 @@
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
 import java.util.List;
+import java.util.Scanner;
 
-public class Subscriber extends Node{
-
+public class Subscriber extends Node {
+    Socket socket;
+    ObjectOutputStream out;
+    ObjectInputStream in;
     public Subscriber( List<Broker> brokers) {
         super(brokers);
     }
 
     public void register(Broker b, Topic t){}
     public void disconnect(Broker b, Topic t){}
-    public void visualiseData(Topic t, Value v){}
+    public void visualiseData(Topic t, Value v) throws IOException, ClassNotFoundException {
+        socket = new Socket(InetAddress.getByName("127.0.0.1"),4321);
+        out = new ObjectOutputStream(socket.getOutputStream());
+        in = new ObjectInputStream(socket.getInputStream());
+        Scanner s = new Scanner(System.in);
+        String i = s.next();
+        System.out.println(i);
+        out.writeObject(i);
+        out.flush();
+        String response = (String) in.readObject();
+        System.out.println("From Sub: item "+i+" is "+ response);
+        socket.close();
+    }
 }
