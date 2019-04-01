7af62893a3367678a91c0f13d07b9c1e5e1fa299
Family <teotsi@gmail.com>
2019-04-01 20:11:53 +0300

    test mains

---
+++ Distributed Systems/test.java
@@ -0,0 +1,9 @@
+import java.io.IOException;
+import java.util.ArrayList;
+public class test {
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        Publisher pub = new Publisher(null);
+        pub.init(4);
+        System.out.println("hey");
+    }
+}
---
+++ Distributed Systems/test2.java
@@ -0,0 +1,8 @@
+import java.io.IOException;
+
+public class test2 {
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        Subscriber sub = new Subscriber(null);
+        sub.visualiseData(null,null);
+    }
+}
