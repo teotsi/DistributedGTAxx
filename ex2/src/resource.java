5dfbf69bbebe6f222703e26a74b75bc1c8d74a73
ChrisCharma <christoscharma@gmail.com>
2019-05-31 17:13:09 +0300

    Remove folder labs

---
+++ ex2/src/resource.java
@@ -0,0 +1,12 @@
+public class resource {
+
+        private String code;
+
+        public resource(String code){
+            this.code = code;
+    }
+
+    public String getCode(){
+        return code;
+        }
+}
