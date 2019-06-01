6487d8a9d65daed8afc2f5285a984055ab739e93
ChrisCharma <christoscharma@gmail.com>
2019-06-01 18:02:38 +0300

    remove locatebusapp

---
+++ labs/lab1/PrintDemo.java
@@ -0,0 +1,11 @@
+package Sychronized_Demo;
+
+public class PrintDemo {
+    public void printcount(){
+        for (int i=5; i>0; i--){
+            System.out.println("Counter ---"+ i);
+        }
+    }
+}
+
+
