a2f5c41ed1133559290004d1f8647316142c8531
Costas Xarhoulacos <p3160121@aueb.gr>
2019-06-01 16:52:49 +0300

    It finally works... Maybe

--- LocateBusApp/app/src/main/java/locatebusapp/Activities/MapsActivity.java
+++ LocateBusApp/app/src/main/java/locatebusapp/Activities/MapsActivity.java
@@ -40,8 +40,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng athens = new LatLng(37.983810, 23.727539);
+        mMap.addMarker(new MarkerOptions().position(athens).title("Marker in Athens"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(athens));
     }
 }
